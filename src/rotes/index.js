a181a1a2f6220505e3c21c23123213450aa81536
walkiria <walkiria.garcia@engenharia.ufjf.br>
2020-08-25 17:31:13 -0300

    ks

--- src/rotes/index.js
+++ src/rotes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/api', (req, res) => {
   res.status(200).send({
     success: 'true',
-    message: 'Seja bem-vindo(a) a API Node.js + PostgreSQL + Azure!',
+    message: 'Seja bem-vindo(a) a API Node.js + PostgreSQL',
     version: '1.0.0',
   });
 });
