9d6f1c74b9069ad173e685fc3a9c6cc23f4f25e5
walkiria <walkiria.garcia@engenharia.ufjf.br>
2020-09-21 17:39:17 -0300

    criptografia de senha de logim

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,13 +1,22 @@
 const db = require('../config/database');
+const bcrypt = require('bcryptjs'); //criptografia de senha
 const express = require('express');
-// ==> Método responsável por criar um novo 'Login':
 
+const saltRounds = 10 //Definindo o valor de caracteres:
+// ==> Método responsável por criar um novo 'Login':
+const salt = bcrypt.genSaltSync(saltRounds)//Criando o salt:
 exports.createLogin = async (req, res) => {
   try {
     const nome = req.body.nome;
     const usuario = req.body.usuario;
     const senha = req.body.senha;
     const cpf = req.body.cpf;
+     
+   // a criptografia é realizada através da aplicação de algoritmos 
+   // matemáticos que modificam qualquer bloco de dados em 
+    //caracteres de comprimento fixos, transformando-os em hashes
+    
+    const hash = bcrypt.hashSync(senha, salt) //Criando o hash:  
 
     const ehUnico = 'SELECT * FROM login WHERE usuario = $1';
     const cpfUnico = 'SELECT * FROM login WHERE cpf = $1';
@@ -23,10 +32,10 @@ exports.createLogin = async (req, res) => {
     });
     const {rows} = await db.query(
       'INSERT INTO login (nome, usuario, senha, cpf) VALUES ($1, $2, $3, $4)',
-      [nome, usuario, senha, cpf],
+      [nome, usuario, hash, cpf],
     );
     res.status(201).send({
-      login: {nome, usuario, senha, cpf},
+      login: {nome, usuario, hash, cpf},
     });
   } catch (err) {
     return res.status(400).send({error: 'Registration failed'});
@@ -42,14 +51,14 @@ exports.listAllLogin = async (req, res) => {
 exports.authenticate = async (req, res) => {
   try {
     const usuario = req.body.usuario;
-    const senha = req.body.senha;
-
+    const senha = req.body.senha; 
+    const hash = bcrypt.hashSync(senha, salt) //Criando o hash:  
     const ehUnico = 'SELECT * FROM login WHERE usuario = $1';
     await db.query(ehUnico, [usuario]).then(function(resultadoDaConsulta) {
       if (resultadoDaConsulta.rows.length == 0) {
         return res.status(400).send({error: 'Usuário Não encontrado'});
       } else {
-        if (resultadoDaConsulta.rows[0].senha !== senha) {
+        if (resultadoDaConsulta.rows[0].senha !== hash) {
           return res.status(400).send({error: 'Senha Inválida'});
         } else {
           res.send(resultadoDaConsulta.rows[0]);
