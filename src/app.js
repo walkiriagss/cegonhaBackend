007c6a631a7ecc601116427ca08f699566e778e9
walkiria <walkiria.garcia@engenharia.ufjf.br>
2021-04-08 14:58:25 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -19,6 +19,17 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.json({type: 'application/vnd.api+json'}));
 app.use(cors({origin: true, credentials: true}));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Headers", "Content-Type"),
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
 app.use(index);
 app.use('/api/', userRoute);
 app.use('/api/', loginRoute);
